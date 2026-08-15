69f6087f733d1b22f686c21d50bd2b924524bf0c
yzgonen <yzgonen@gmail.com>
2026-08-16 00:40:30 +0100

    Register animated mouth player render layer

--- src/main/java/com/vanguard/lipsync/client/VanguardLipSyncClient.java
+++ src/main/java/com/vanguard/lipsync/client/VanguardLipSyncClient.java
@@ -1,10 +1,22 @@
 package com.vanguard.lipsync.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.LivingEntityFeatureRendererRegistrationCallback;
+import net.minecraft.client.render.entity.PlayerEntityRenderer;
+import net.minecraft.client.render.entity.feature.FeatureRendererContext;
+import net.minecraft.client.render.entity.model.PlayerEntityModel;
+import net.minecraft.client.render.entity.state.PlayerEntityRenderState;
 
 public final class VanguardLipSyncClient implements ClientModInitializer {
     @Override
+    @SuppressWarnings({"rawtypes", "unchecked"})
     public void onInitializeClient() {
-        // Voice integration is initialized through Simple Voice Chat's plugin entrypoint.
+        LivingEntityFeatureRendererRegistrationCallback.EVENT.register((entityType, entityRenderer, helper, context) -> {
+            if (entityRenderer instanceof PlayerEntityRenderer<?> playerRenderer) {
+                helper.register(new MouthFeatureRenderer(
+                        (FeatureRendererContext<PlayerEntityRenderState, PlayerEntityModel>) (FeatureRendererContext) playerRenderer
+                ));
+            }
+        });
     }
 }
