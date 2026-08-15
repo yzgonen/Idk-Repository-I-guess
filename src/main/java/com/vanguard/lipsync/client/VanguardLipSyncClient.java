0a0f320dca496e97d2bb891e23da7d64c3d1ced4
yzgonen <yzgonen@gmail.com>
2026-08-16 00:24:48 +0100

    Add Fabric client entrypoint

---
+++ src/main/java/com/vanguard/lipsync/client/VanguardLipSyncClient.java
@@ -0,0 +1,10 @@
+package com.vanguard.lipsync.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public final class VanguardLipSyncClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // Voice integration is initialized through Simple Voice Chat's plugin entrypoint.
+    }
+}
