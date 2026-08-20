db419b3b575e83c7a7dc8fc6b6f3b8279faf992a
yzgonen <yzgonen@gmail.com>
2026-08-20 14:13:59 +0100

    Add backslash X-Ray toggle

--- player-chest-esp/src/main/java/com/example/simpleesp/SimpleEspClient.java
+++ player-chest-esp/src/main/java/com/example/simpleesp/SimpleEspClient.java
@@ -5,6 +5,9 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.ShulkerBoxBlock;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.ChestBlockEntity;
 import net.minecraft.block.entity.EnderChestBlockEntity;
@@ -20,6 +23,7 @@ import net.minecraft.client.util.InputUtil;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
@@ -34,9 +38,11 @@ public final class SimpleEspClient implements ClientModInitializer {
 
     private static KeyBinding togglePlayers;
     private static KeyBinding toggleChests;
+    private static KeyBinding toggleXray;
 
     private static boolean playerEsp = true;
     private static boolean chestEsp = true;
+    private static volatile boolean xrayEnabled = false;
 
     private static final int CHEST_SCAN_CHUNK_RADIUS = 8;
     private static final double MAX_PLAYER_DISTANCE_SQ = 256.0 * 256.0;
@@ -57,6 +63,13 @@ public final class SimpleEspClient implements ClientModInitializer {
                 "category.simpleesp"
         ));
 
+        toggleXray = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.simpleesp.toggle_xray",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_BACKSLASH,
+                "category.simpleesp"
+        ));
+
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (togglePlayers.wasPressed()) {
                 playerEsp = !playerEsp;
@@ -65,6 +78,13 @@ public final class SimpleEspClient implements ClientModInitializer {
             while (toggleChests.wasPressed()) {
                 chestEsp = !chestEsp;
                 hud("Chest ESP: " + (chestEsp ? "ON" : "OFF"));
+            }
+            while (toggleXray.wasPressed()) {
+                xrayEnabled = !xrayEnabled;
+                hud("X-Ray: " + (xrayEnabled ? "ON" : "OFF"));
+                if (client.worldRenderer != null) {
+                    client.worldRenderer.reload();
+                }
             }
         });
 
@@ -102,6 +122,38 @@ public final class SimpleEspClient implements ClientModInitializer {
             RenderSystem.disableBlend();
             matrices.pop();
         });
+    }
+
+    public static boolean isXrayEnabled() {
+        return xrayEnabled;
+    }
+
+    public static boolean shouldRenderInXray(BlockState state) {
+        if (state.isIn(BlockTags.COAL_ORES)
+                || state.isIn(BlockTags.IRON_ORES)
+                || state.isIn(BlockTags.COPPER_ORES)
+                || state.isIn(BlockTags.GOLD_ORES)
+                || state.isIn(BlockTags.REDSTONE_ORES)
+                || state.isIn(BlockTags.LAPIS_ORES)
+                || state.isIn(BlockTags.DIAMOND_ORES)
+                || state.isIn(BlockTags.EMERALD_ORES)) {
+            return true;
+        }
+
+        if (state.getBlock() instanceof ShulkerBoxBlock) {
+            return true;
+        }
+
+        return state.isOf(Blocks.NETHER_GOLD_ORE)
+                || state.isOf(Blocks.NETHER_QUARTZ_ORE)
+                || state.isOf(Blocks.ANCIENT_DEBRIS)
+                || state.isOf(Blocks.SPAWNER)
+                || state.isOf(Blocks.TRIAL_SPAWNER)
+                || state.isOf(Blocks.VAULT)
+                || state.isOf(Blocks.CHEST)
+                || state.isOf(Blocks.TRAPPED_CHEST)
+                || state.isOf(Blocks.ENDER_CHEST)
+                || state.isOf(Blocks.BARREL);
     }
 
     private static void renderPlayers(ClientWorld world, BufferBuilder buffer, Matrix4f matrix) {
